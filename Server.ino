185c33c9981d1ac5193c205efe6c22f975c506ac
L <ryuzaky@gmail.com>
2020-02-29 16:05:04 +0100

    add an initial script for the server

---
+++ Server.ino
@@ -0,0 +1,11 @@
+//this sets the ground pin to LOW and the input voltage pin to high
+void setup() {
+    Serial.begin(9600); 
+}
+
+void loop()       //main loop
+
+{
+    Serial.println("Hello World!!!");
+    delay(1000);
+}
